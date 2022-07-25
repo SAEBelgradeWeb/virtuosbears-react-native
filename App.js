758c70142a1dde50da226b8b99a96547ff6d1b26
nlukic97 <nlukic97@gmail.com>
2022-07-25 19:45:41 +0200

    1st class

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Header from "./components/Header"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.view}>
+      <Header title={"My website"}/>
+      <Text style={styles.text}>Hello world!</Text>
+      <Image source={{uri:"https://randomuser.me/portraits/men/1.jpg"}} style={styles.img} />
     </View>
   );
 }
 
+Header.defaultProps ={
+  title:"No title"
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  view:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:"center"
   },
-});
+  text:{
+    color:"darkslateblue",
+    fontSize:30
+  },
+  img:{
+    width:100,
+    height:100,
+    borderRadius:100/2 // !!!
+  }
+})
---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+export default function Header({title}){
+    return(
+        <Text>{title}</Text>
+    )
+}
